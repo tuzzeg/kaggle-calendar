c588e482f74f4612f9f29ca8f3a072cfb75f0a08
Dmitry <deemonster@gmail.com>
2014-09-29 18:14:28 -0700

    Add proto for competition data. Parse competition page.

--- kaggle_list.py
+++ kaggle_list.py
@@ -5,33 +5,22 @@ from bs4 import BeautifulSoup
 from httpcache import CachedHttp
 import re
 from datetime import datetime
+from data_pb2 import Competition, Date
+import calendar
 
 httpCache = 'd/html.kch'
 
 allCompetitionsUrl = "http://www.kaggle.com/competitions/search?SearchVisibility=AllCompetitions&ShowActive=true&ShowCompleted=true&ShowProspect=true&ShowOpenToAll=true&ShowPrivate=true&ShowLimited=true"
 
-Competition = namedtuple('Competetion', ['url'])
 def extractCompetitions(html):
   doc = BeautifulSoup(html)
   for el in doc.body.find_all('div', attrs={'class': 'competition-details'}):
     el_p = el.find('p', attrs={'class': 'competition-summary'})
     el_p_a = el_p.find('a')
-    yield Competition(url=el_p_a['href'])
 
-def printCompetitions(http):
-  html = http.get(allCompetitionsUrl)
-  for c in extractCompetitions(html):
-    url = urlparse.urljoin('http://kaggle.com', c.url)
-    print url
-    #html = http.get(url)
-    #print html
-
-def parseCompetition(http, url):
-  html = http.get(url)
-  doc = BeautifulSoup(html)
-
-  for el in doc.body.find_all('p', attrs={'id': 'end-time-note'}):
-    print el.text
+    id = el_p_a['href']
+    url = urlparse.urljoin('http://kaggle.com', id)
+    yield Competition(id=id, url=url)
 
 class ReFactory(object):
   def __init__(self, terminals):
@@ -61,12 +50,48 @@ def extractDates(text):
     date = datetime.strptime(dateStr, '%I:%M %p, %A %d %B %Y %Z')
     yield (typ.lower(), date)
 
+def updateCompetition(html, competition):
+  doc = BeautifulSoup(html)
+
+  _updateCompetitionTitle(doc, competition)
+  _updateCompetitionDates(doc, competition)
+
+def _updateCompetitionTitle(doc, competition):
+  el = doc.body.find('div', attrs={'id': 'comp-header-details'})
+  el_h1 = el.find('h1')
+  if el_h1:
+    competition.title = el_h1.text.strip()
+
+def _updateCompetitionDates(doc, competition):
+  el = doc.body.find('p', attrs={'id': 'end-time-note'})
+  dates = dict(extractDates(el.text))
+
+  if 'started' in dates:
+    _setDate(competition.start, dates['started'])
+  if 'ended' in dates:
+    _setDate(competition.end, dates['ended'])
+
+def _setDate(proto, dt):
+  proto.timestamp_utc = calendar.timegm(dt.utctimetuple())
+
+def _dumpCompetitions(http):
+  html = http.get(allCompetitionsUrl)
+  for c in extractCompetitions(html):
+    print c
+
+def _dumpCompetition(http, url):
+  html = http.get(url)
+
+  c = Competition(id='1')
+  updateCompetition(html, c)
+
+  print c
+
 def main():
   http = CachedHttp(httpCache)
 
-  # printCompetitions(http)
-
-  parseCompetition(http, 'http://www.kaggle.com/c/mdm')
+  # _dumpCompetitions(http)
+  _dumpCompetition(http, 'http://www.kaggle.com/c/mdm')
 
 if __name__ == '__main__':
   main()
