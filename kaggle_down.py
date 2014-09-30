9a7ac699289dbda87ab927e68eb83f37e8b5d176
Dmitry <deemonster@gmail.com>
2014-09-29 21:20:22 -0700

    Add downloader with rate limit.

---
+++ kaggle_down.py
@@ -0,0 +1,28 @@
+from httpcache import CachedHttp
+from kaggle_list import extractCompetitions
+import time
+
+httpCache = 'd/html.kch'
+
+allCompetitionsUrl = "http://www.kaggle.com/competitions/search?SearchVisibility=AllCompetitions&ShowActive=true&ShowCompleted=true&ShowProspect=true&ShowOpenToAll=true&ShowPrivate=true&ShowLimited=true"
+
+downloadTimeoutSec = 1
+
+def downloadCompetitions(http):
+  html = http.get(allCompetitionsUrl)
+
+  # we don't want to be banned by Kaggle, make 1 request per second
+  t0 = time.time()
+  for c in extractCompetitions(html):
+    while time.time()-t0 < downloadTimeoutSec:
+      time.sleep(0.1)
+
+    print '  download [%s]' % c.url
+    http.get(c.url)
+
+def main():
+  http = CachedHttp(httpCache)
+  downloadCompetitions(http)
+
+if __name__ == '__main__':
+  main()
