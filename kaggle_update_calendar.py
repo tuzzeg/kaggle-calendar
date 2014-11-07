7f9c48da06e68d6ce7c8f152d18e8295cef82d1b
Dmitry <deemonster@gmail.com>
2014-11-06 23:42:19 -0800

    Fix kaggle_update_calendar to use refresh token.

--- kaggle_update_calendar.py
+++ kaggle_update_calendar.py
@@ -1,17 +1,20 @@
-from data_pb2 import Competition, Date, CalendarSyncerConfig
+from data_pb2 import Competition, Date
 from functools import partial
 from lib import cmd
 from lib.cmd import Command
 from lib.files import readFile
-from oauth2client.client import SignedJwtAssertionCredentials
-import apiclient
+from oauth import oauthSession
 import base64
-import httplib2
 import logging
+import requests
 import sqlite3
 import time
+import urllib
+import json
 
 logger = logging.getLogger(__name__)
+
+BASE_CALENDAR_URL = 'https://www.googleapis.com/calendar/v3'
 
 def _sync(conf, id=None):
   if id is None:
@@ -20,28 +23,16 @@ def _sync(conf, id=None):
     competitionF = lambda competition: competition.id == id
 
   syncer = CalendarSyncer(conf)
-  syncer.authenticate()
   syncer.syncEvents(conf.sqliteSyncer.sqliteFile, competitionF)
 
 class CalendarSyncer(object):
   def __init__(self, conf):
     self.conf = conf.calendarSyncer
-    self._service = None
 
-  def authenticate(self):
-    conf = self.conf.authentication
-
-    http = httplib2.Http()
-
-    credentials = SignedJwtAssertionCredentials(
-      conf.serviceAccount.email,
-      readFile(conf.serviceAccount.keysFile),
-      scope='https://www.googleapis.com/auth/calendar')
-    http = credentials.authorize(http)
-
-    self._service = apiclient.discovery.build(
-      serviceName='calendar', version='v3', http=http,
-      developerKey=conf.developersKey)
+    scope = [
+      'https://www.googleapis.com/auth/calendar',
+    ]
+    self.google = oauthSession(conf, scope)
 
   def syncEvents(self, sqliteFile, competitionF):
     calendars = {
@@ -64,26 +55,40 @@ class CalendarSyncer(object):
   def _syncCompetition(self, calendarId, competition):
     eventId = _eventId(competition)
     try:
-      event = self._service.events().get(calendarId=calendarId, eventId=eventId).execute()
+      event = self._getEvent(calendarId, eventId)
       if _match(competition, event):
         logger.debug('[%s] no upate' % (competition.id))
       else:
-        logger.debug('[%s] update' % (competition.id))
-        self._updateEvent(calendarId, competition)
-    except apiclient.errors.HttpError, e:
-      if e.resp.status == 404:
+        if event['status'] == 'cancelled':
+          logger.debug('[%s] insert' % (competition.id))
+          self._addEvent(calendarId, competition)
+        else:
+          logger.debug('[%s] update' % (competition.id))
+          self._updateEvent(calendarId, competition)
+    except requests.exceptions.HTTPError, e:
+      if e.response.status_code == 404:
         logger.debug('[%s] insert' % (competition.id))
         self._addEvent(calendarId, competition)
       else:
         raise e
 
+  def _getEvent(self, calendarId, eventId):
+    url = '%s/calendars/%s/events/%s' % (BASE_CALENDAR_URL, urllib.quote(calendarId), urllib.quote(eventId))
+    r = self.google.get(url)
+    r.raise_for_status()
+    return r.json()
+
   def _addEvent(self, calendarId, competition):
     event = _event(competition)
-    ev = self._service.events().insert(calendarId=calendarId, body=event).execute()
+    url = '%s/calendars/%s/events' % (BASE_CALENDAR_URL, urllib.quote(calendarId))
+    headers = {'content-type': 'application/json'}
+    self.google.post(url, data = json.dumps(event), headers=headers)
 
   def _updateEvent(self, calendarId, competition):
     event = _event(competition)
-    ev = self._service.events().update(calendarId=calendarId, eventId=event['id'], body=event).execute()
+    url = '%s/calendars/%s/events/%s' % (BASE_CALENDAR_URL, urllib.quote(calendarId), urllib.quote(event['id']))
+    headers = {'content-type': 'application/json'}
+    self.google.put(url, data = json.dumps(event), headers=headers)
 
 def _match(competition, event):
   return _event(competition) == event
@@ -103,7 +108,7 @@ def _event(competition):
   return event
 
 def _eventId(competition):
-  return base64.b16encode(competition.id).lower()
+  return base64.b16encode('cal:' + competition.id).lower()
 
 def _jsonTime(dt):
   return {
